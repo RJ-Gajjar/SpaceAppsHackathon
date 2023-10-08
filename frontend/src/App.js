7bf7e6547547cf8e40751460ba16f6ae02341745
mnkjoshi <manavjoshi18@gmail.com>
2023-10-08 04:30:31 -0600

    Front end push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
+import './App.css'
+
+const user = {};
+user.name = "Manav";
+
+function MyButton() {
+
+  function ButtonClick() {
+    alert("Clicked!")
+  } 
+
+  return ( 
+    <button onClick={ButtonClick} className='button'>
+      I am a button!
+    </button>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1> Welcome to </h1>
+      <h1 className = "title">  Welcome to website <br/> Wanna chill, {user.name}?</h1>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <MyButton/>
+      
     </div>
   );
 }
